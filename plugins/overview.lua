4baebd35f7c6aebbd0c6b6c750a1abd4cde859f8
alimessibot <aliamiralizadebot@gmail.com>
2016-05-06 10:16:01 +0430

    Update overview.lua

--- plugins/overview.lua
+++ plugins/overview.lua
@@ -2,7 +2,7 @@ do
  function run(msg, matches)
 if is_momod(msg) then
 return [[ 
-avast group manager robot status:Protection active,no background jobs using system
+Telekiller group manager robot status:Protection active,no background jobs using system
 ]]
 end
 end
